a5c9b1ab270c812661fe392034c0a6f95a08b4de
cornelia chirila <cornelia.calarasiu@yahoo.it>
2020-10-16 22:15:36 +0100

    react giphy

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,3 @@
+/* eslint-disable no-dupe-keys */
 // eslint-disable-next-line no-unused-vars
-import React, { Component } from 'react';
-import SearchBar from './components/SearchBar';
-import Gif from './components/Gif';
-import GifList from './components/GifList';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <div className="left-scene">
-          <SearchBar />
-          {/* <Gif /> */}
-        </div>
-        <div className="right-scene">
-          {/* <GifList /> */}
-        </div>
-      </div>
-    );
-  }
-}
-
-export default App;
