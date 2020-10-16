6c0a581d891b1e677b5d34a58a72af322007c6f0
cornelia chirila <cornelia.calarasiu@yahoo.it>
2020-10-16 22:21:23 +0100

    undo the changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
 /* eslint-disable no-dupe-keys */
 // eslint-disable-next-line no-unused-vars
+import React, { Component } from 'react';
+import giphy from 'giphy-api';
+import SearchBar from './components/SearchBar';
+import Gif from './components/Gif';
+import GifList from './components/GifList';
 
+// eslint-disable-next-line react/prefer-stateless-function
+
+
+export default App;
